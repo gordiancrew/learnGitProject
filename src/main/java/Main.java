52983c22c6ec6f1732b68511cb930b900afa8871
Vladimir Kazantsev <gordiancrew@gmail.com>
2022-04-23 19:21:11 +0300

    Revert "Initial commit"
    This reverts commit e78b9d97

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, stage 1");
-        System.out.println("Commit 1");
-        System.out.println("stage4");
+        System.out.println("stage5");
     }
 }
