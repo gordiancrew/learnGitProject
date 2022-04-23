ca2d63d33ecdebbb80568d19faef106620de78e2
Vladimir Kazantsev <gordiancrew@gmail.com>
2022-04-23 21:30:58 +0300

    commit8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("stage5");
         System.out.println("stage6");
+        System.out.println("Stage20");
+        System.out.println("Branch1 Commit");
     }
 }
