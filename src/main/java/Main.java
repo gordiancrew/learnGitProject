1615bf1d7924fbaa4dfa005466c271c359d560fb
Vladimir Kazantsev <gordiancrew@gmail.com>
2022-04-23 21:28:28 +0300

    commit8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("stage5");
-        System.out.println("stage6");
+        System.out.println("Hello, stage 1");
+        System.out.println("Stage10");
+        System.out.println("Stage11");
     }
 }
